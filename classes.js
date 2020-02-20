55f3a2d9d58738e3c92024b9dcbfc692915c2543
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-02-20 17:15:16 +1300

    add function makeVisible. Call function in start

--- classes.js
+++ classes.js
@@ -10,6 +10,8 @@ function start () {
   two ()
     
   three ()
+
+  makeVisible ()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -32,3 +34,19 @@ function three () {
   three.classList.add('red')
 }
 // CREATE FUNCTION makeVisible HERE
+/*Notice that the fourth div in index.html has a class already: invisible. It's also not on the screen in the browser. There's no id attribute, so we can't find it using getElementById. To find things by class instead, we need to use getElementsByClassName (note the 's' in Elements).
+
+getElementsByClassName returns an array, because there could be many DOM elements with the class invisible. The MDN documentation on getElementsByClassName might be useful here.
+
+Write a new function called makeVisible. Because there's only one invisible div, we know it must be element [0] in the array that comes back from getElementsByClassName.
+
+Add the 'visible' class to the div's classList property using the same method as the previous examples.
+
+Call the makeVisible function from start.
+
+Reload the browser. If all has gone well, you should see a fourth div. If not, spend a little time troubleshooting, then reach out for help on slack.*/
+
+function makeVisible () {
+  var visible = document.getElementsByClassName('invisible')
+  visible[0].classList.add('visible')
+}
